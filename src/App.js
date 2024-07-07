32ec767a406b4c0e826127fb73b4a9df48a9e7fc
samrit0001 <samrit098@gmail.com>
2024-07-07 19:38:19 +0530

    Header for jeff application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="upload-page">
+      <header className="header">
+        <div className="left-side">
+          <div className="profile-pic"> 
+            <img src={"profile-icon.svg"} alt="icon" />
+          </div>
+          <div className="camera-icon">
+            <img src={"add-video.svg"} alt="add-video-icon" />
+          </div>
+        </div>
+      <div className="search-bar">
+        <input  className="search-box" type="text" placeholder="Search Challenge Collection"  />
+      </div>
+      <div className ="wallet-button">
+        <button className = "Connect-wallet">Connect Wallet</button>
+      </div>
+    </header>
     </div>
   );
 }
